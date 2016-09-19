391287c4cbce567ddf7b0b777ed61c08ee4cb6c2
Theodore Quinn <Theodore Quinn>
2016-09-18 21:18:33 -0400

    Change search request from https back to http

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 		var searchTerm = $('#searchBox').val();
 		$('#searchBox').val('');
 		$.ajax({
-			url: "https://www.canistream.it/services/search",
+			url: "http://www.canistream.it/services/search",
 			data: {
 				movieName: searchTerm
 			},
